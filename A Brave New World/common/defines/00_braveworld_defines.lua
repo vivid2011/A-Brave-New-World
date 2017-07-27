fa87aaf4396ae02193f656181c330687d110d1da
MICHAELSPC\Michael <michaelgardone@gmail.com>
2017-07-26 23:36:15 -0400

    End date for mod pushed to 2035, changed some of the OOBs around

--- A Brave New World/common/defines/00_braveworld_defines.lua
+++ A Brave New World/common/defines/00_braveworld_defines.lua
@@ -2,7 +2,7 @@ NDefines = {
 
 NGame = {
 	START_DATE = "1900.1.1.12",
-	END_DATE = "2020.1.1.1",
+	END_DATE = "2035.12.31.23",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,	-- Yes, we did the math
 	SAVE_VERSION = 3,
 	LAG_DAYS_FOR_LOWER_SPEED = 10,		-- Days of client lag for decrease of gamespeed
